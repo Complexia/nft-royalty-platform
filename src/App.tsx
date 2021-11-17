27a697d5da4186312358b45b80c0f2d7aad16c82
Complexia <complexia701@gmail.com>
2021-11-17 12:56:12 +1100

    Boilerplate React interface. Next step to connect wallet

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Switch from 'react-bootstrap/esm/Switch';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import HomePage from './pages/HomePage';
+import NavBar from './components/Navbar';
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className="dome">
+
+      <BrowserRouter >
+          <NavBar />
+          <Switch className="leftVerticalLine">
+            
+            <Route path="/" component={ HomePage } exact />
+            
+
+          </Switch>
+        </BrowserRouter>
     </div>
-  );
+
+  )
 }
 
 export default App;
